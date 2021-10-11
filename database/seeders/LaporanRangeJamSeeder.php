a3376de9c0412f7a3a24902c37220549bf557988
root <alvin.faiz@primakom.co.id>
2021-10-11 16:19:56 +0700

    tambah seeder, alter range jam

--- database/seeders/LaporanRangeJamSeeder.php
+++ database/seeders/LaporanRangeJamSeeder.php
@@ -18,10 +18,19 @@ class LaporanRangeJamSeeder extends Seeder
         $abjad = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Q', 'Y', 'Z'];
 
         for($i=0;$i<=23;$i++) {
+            $kodeshift = '-';
+            if(($i==0) || ($i<=7)) {
+                $kodeshift = 'M';
+            } else if(($i>=8) && ($i<=15)) {
+                $kodeshift = 'P';
+            } else if(($i>=16) && ($i<=23)) {
+                $kodeshift = 'S';
+            }
             LaporanRangeJam::create([
                 'uuid'  => generateUuid(),
                 'time' => $i.':00',
                 'kode' => $abjad[$i],
+                'kode_shift' => $kodeshift,
                 'created_at' => date('Y-m-d H:i:s')
             ]);
         }
